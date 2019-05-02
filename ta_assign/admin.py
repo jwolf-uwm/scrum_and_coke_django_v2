98e30ae969c989f3215d4994a97a62ff5b2501ec
evanv <evetrano@uwm.edu>
2019-05-02 12:39:17 -0500

    course changes

--- ta_assign/admin.py
+++ ta_assign/admin.py
@@ -3,9 +3,14 @@ from django.contrib import admin
 # Register your models here.
 from .models import (User)
 from .models import (Course)
-from .models import (TACourse)
+from .models import (Lecture)
 from .models import (Lab)
+from .models import (InstructorCourse)
+from .models import (TACourse)
 admin.site.register(User)
 admin.site.register(Course)
-admin.site.register(TACourse)
+admin.site.register(Lecture)
 admin.site.register(Lab)
+admin.site.register(InstructorCourse)
+admin.site.register(TACourse)
+
--- ta_assign/models.py
+++ ta_assign/models.py
@@ -12,18 +12,35 @@ class User(models.Model):
 
 
 class Course(models.Model):
-    course_id = models.CharField(max_length=10)
-    num_labs = models.IntegerField(default=0)
-    instructor = models.CharField(max_length=50, default="no Instructor")
-    current_num_TA = models.IntegerField(default=0)
-    # temp disabled
-    # tee_ays = models.ForeignKey(ModelTA, on_delete=models.CASCADE)
+    course_department = models.CharField(max_length=6)
+    course_id = models.PositiveSmallIntegerField(default=0)
+    num_lectures = models.PositiveSmallIntegerField(default=0)
+    num_labs = models.PositiveSmallIntegerField(default=0)
+    num_graders = models.PositiveSmallIntegerField(default=0)
+    current_num_TA = models.PositiveSmallIntegerField(default=0)
+    current_num_lectures = models.PositiveSmallIntegerField(default=0)
+    current_num_labs = models.PositiveSmallIntegerField(default=0)
+
+
+class Lecture(models.Model):
+    instructor = models.CharField(max_length=50, default="no instructor")
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    lecture_section = models.PositiveSmallIntegerField(default=0)
+    lecture_location = models.CharField(max_length=50)
+    lecture_time = models.TimeField()
 
 
 class Lab(models.Model):
     TA = models.CharField(max_length=50, default="no TA")
-    section_id = models.IntegerField(default=0)
-    course_id = models.CharField(max_length=10)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    lab_section = models.PositiveSmallIntegerField(default=0)
+    lab_location = models.CharField(max_length=50)
+    lab_time = models.TimeField()
+
+
+class InstructorCourse(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    instructor = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 class TACourse(models.Model):
