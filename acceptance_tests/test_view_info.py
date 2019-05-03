2698b62753157474ab201a32d1b0cadb26a8b7ed
Jeffrey Wolf <lwolf@uwm.edu>
2019-05-02 22:54:01 -0500

    1337 multi-user test

--- acceptance_tests/test_view_info.py
+++ acceptance_tests/test_view_info.py
@@ -105,3 +105,74 @@ class ViewInfoTests(TestCase):
         self.assertContains(response, "ta@uwm.edu")
         all_messages = [msg for msg in get_messages(response.wsgi_request)]
         self.assertEqual(len(all_messages), 0)
+
+    def test_multi_users(self):
+
+        ad1 = models.User()
+        ad1.email = "ta_assign_admin@uwm.edu"
+        ad1.password = "password"
+        ad1.type = "administrator"
+        ad1.save()
+
+        sup1 = models.User()
+        sup1.email = "ta_assign_super@uwm.edu"
+        sup1.password = "password"
+        sup1.type = "supervisor"
+        sup1.save()
+
+        client = Client()
+        session = client.session
+        session['email'] = 'ta_assign_admin@uwm.edu'
+        session['type'] = 'administrator'
+        session.save()
+
+        response = client.get('/edit_info/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Edit Info")
+        self.assertContains(response, "Current Email:")
+        self.assertContains(response, "ta_assign_admin@uwm.edu")
+        all_messages = [msg for msg in get_messages(response.wsgi_request)]
+        self.assertEqual(len(all_messages), 0)
+
+    def test_multi_users_online(self):
+
+        ad1 = models.User()
+        ad1.email = "ta_assign_admin@uwm.edu"
+        ad1.password = "password"
+        ad1.type = "administrator"
+        ad1.save()
+
+        sup1 = models.User()
+        sup1.email = "ta_assign_super@uwm.edu"
+        sup1.password = "password"
+        sup1.type = "supervisor"
+        sup1.save()
+
+        client = Client()
+        session = client.session
+        session['email'] = 'ta_assign_admin@uwm.edu'
+        session['type'] = 'administrator'
+        session.save()
+
+        client2 = Client()
+        session2 = client2.session
+        session2['email'] = 'ta_assign_super@uwm.edu'
+        session2['type'] = 'supervisor'
+        session2.save()
+
+        response = client.get('/edit_info/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Edit Info")
+        self.assertContains(response, "Current Email:")
+        self.assertContains(response, "ta_assign_admin@uwm.edu")
+        all_messages = [msg for msg in get_messages(response.wsgi_request)]
+        self.assertEqual(len(all_messages), 0)
+
+        response2 = client2.get('/edit_info/')
+        self.assertEqual(response2.status_code, 200)
+        self.assertContains(response2, "Edit Info")
+        self.assertContains(response2, "Current Email:")
+        self.assertContains(response2, "ta_assign_super@uwm.edu")
+        all_messages2 = [msg for msg in get_messages(response2.wsgi_request)]
+        self.assertEqual(len(all_messages2), 0)
+
