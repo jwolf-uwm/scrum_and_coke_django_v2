ff665dddb70dae26ae5e33e7eda74e788007213a
MHibbard77 <hibbard3@uwm.edu>
2019-05-05 20:37:29 -0500

    assigns progress

--- acceptance_tests/test_assign_ta_lablec.py
+++ acceptance_tests/test_assign_ta_lablec.py
@@ -944,6 +944,11 @@ class AssignTaTests(TestCase):
         inscourse.course = course
         inscourse.save()
 
+        inscourse1= models.InstructorCourse()
+        inscourse1.instructor = inst1
+        inscourse1.course = course1
+        inscourse1.save()
+
         tacourse = models.TACourse()
         tacourse.TA = ta1
         tacourse.course = course
@@ -964,3 +969,177 @@ class AssignTaTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Assign TA to Labs/Lectures")
         self.assertContains(response, "TA not assigned to this course!")
+
+    def test_no_lab_or_lec_ins(self):
+        ta1 = models.User()
+        ta1.email = "ta@uwm.edu"
+        ta1.type = "ta"
+        ta1.save()
+
+        inst1 = models.User()
+        inst1.email = "instructor@uwm.edu"
+        inst1.type = "instructor"
+        inst1.save()
+
+        course = models.Course()
+        course.num_labs = 2
+        course.current_num_TA = 1
+        course.num_lectures = 1
+        course.course_id = "301"
+        course.course_department = "COMPSCI"
+        course.save()
+
+        inscourse = models.InstructorCourse()
+        inscourse.instructor = inst1
+        inscourse.course = course
+        inscourse.save()
+
+        tacourse = models.TACourse()
+        tacourse.TA = ta1
+        tacourse.course = course
+        tacourse.save()
+
+        lec = models.Lab()
+        lec.course = course
+        lec.lab_section = "801"
+        lec.save()
+        client = Client()
+        session = client.session
+        session['email'] = 'instructor@uwm.edu'
+        session['type'] = 'instructor'
+        session.save()
+        response = client.post('/assign_ta_lablec/', data={'email': "ta@uwm.edu", 'course_id': "301",
+                                                           'course_department': "COMPSCI", 'course_section': "804"},
+                               follow="true")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Assign TA to Labs/Lectures")
+        self.assertContains(response, "No Such Lab or Lecture")
+
+    def test_no_such_lab_or_lec_sup(self):
+        ta1 = models.User()
+        ta1.email = "ta@uwm.edu"
+        ta1.type = "ta"
+        ta1.save()
+
+        sup1 = models.User()
+        sup1.email = "ta_assign_super@uwm.edu"
+        sup1.type = "supervisor"
+        sup1.save()
+
+        course = models.Course()
+        course.num_labs = 0
+        course.current_num_TA = 0
+        course.num_lectures = 1
+        course.instructor = "DEFAULT"
+        course.course_id = "301"
+        course.course_department = "COMPSCI"
+        course.save()
+
+        tacourse = models.TACourse()
+        tacourse.TA = ta1
+        tacourse.course = course
+        tacourse.save()
+
+        lec = models.Lecture()
+        lec.course = course
+        lec.lecture_section = "401"
+        lec.save()
+
+        client = Client()
+        session = client.session
+        session['email'] = 'ta_assign_super@uwm.edu'
+        session['type'] = 'supervisor'
+        session.save()
+        response = client.post('/assign_ta_lablec/', data={'email': "ta@uwm.edu", 'course_id': "301",
+                                                           'course_department': "COMPSCI", 'course_section': "404"},
+                               follow="true")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Assign TA")
+        self.assertContains(response, "No Such Lab or Lecture")
+
+    def test_invalid_lec_assign_sup(self):
+        ta1 = models.User()
+        ta1.email = "ta@uwm.edu"
+        ta1.type = "ta"
+        ta1.save()
+
+        sup1 = models.User()
+        sup1.email = "ta_assign_super@uwm.edu"
+        sup1.type = "supervisor"
+        sup1.save()
+
+        course = models.Course()
+        course.num_labs = 2
+        course.current_num_TA = 0
+        course.num_lectures = 1
+        course.instructor = "DEFAULT"
+        course.course_id = "301"
+        course.course_department = "COMPSCI"
+        course.save()
+
+        tacourse = models.TACourse()
+        tacourse.TA = ta1
+        tacourse.course = course
+        tacourse.save()
+
+        lec = models.Lecture()
+        lec.course = course
+        lec.lecture_section = "401"
+        lec.save()
+
+        client = Client()
+        session = client.session
+        session['email'] = 'ta_assign_super@uwm.edu'
+        session['type'] = 'supervisor'
+        session.save()
+        response = client.post('/assign_ta_lablec/', data={'email': "ta@uwm.edu", 'course_id': "301",
+                                                           'course_department': "COMPSCI", 'course_section': "401"},
+                               follow="true")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Assign TA")
+        self.assertContains(response, "TA cannot be assigned to this lecture(labs exist)!")
+
+    def test_invalid_lec_assign_ins(self):
+        ta1 = models.User()
+        ta1.email = "ta@uwm.edu"
+        ta1.type = "ta"
+        ta1.save()
+
+        inst1 = models.User()
+        inst1.email = "instructor@uwm.edu"
+        inst1.type = "instructor"
+        inst1.save()
+
+        course = models.Course()
+        course.num_labs = 2
+        course.current_num_TA = 1
+        course.num_lectures = 1
+        course.course_id = "301"
+        course.course_department = "COMPSCI"
+        course.save()
+
+        inscourse = models.InstructorCourse()
+        inscourse.instructor = inst1
+        inscourse.course = course
+        inscourse.save()
+
+        tacourse = models.TACourse()
+        tacourse.TA = ta1
+        tacourse.course = course
+        tacourse.save()
+
+        lec = models.Lecture()
+        lec.course = course
+        lec.lecture_section = "401"
+        lec.save()
+        client = Client()
+        session = client.session
+        session['email'] = 'instructor@uwm.edu'
+        session['type'] = 'instructor'
+        session.save()
+        response = client.post('/assign_ta_lablec/', data={'email': "ta@uwm.edu", 'course_id': "301",
+                                                           'course_department': "COMPSCI", 'course_section': "401"},
+                               follow="true")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Assign TA to Labs/Lectures")
+        self.assertContains(response, "TA cannot be assigned to this lecture(labs exist)!")
