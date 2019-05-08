dd176ddbf6c3dcb702fdc593e38bd67ed943bd38
MHibbard77 <hibbard3@uwm.edu>
2019-05-08 13:19:23 -0500

    assigns progress

--- acceptance_tests/test_view_course_assignments.py
+++ acceptance_tests/test_view_course_assignments.py
@@ -77,12 +77,84 @@ class ViewCourseAssignmentsTests(TestCase):
         session['type'] = 'supervisor'
         # save the session
         session.save()
-        response = client.get('/assign_instructor/')
+        response = client.get('/view_course_assignments/')
+        all_messages = [msg for msg in get_messages(response.wsgi_request)]
+        self.assertEqual(all_messages[0].tags, "error")
+        self.assertEqual(all_messages[0].message, "You do not have access to this page.")
+        self.assertEqual(response.get('location'), '/index/')
+
+    def test_add_one_course(self):
+        inst1 = models.User()
+        inst1.email = "inst@uwm.edu"
+        inst1.type = "instructor"
+        inst1.save()
+
+        course = models.Course()
+        course.course_department = "COMPSCI"
+        course.course_id = "301"
+        course.save()
+
+        insCourse = models.InstructorCourse()
+        insCourse.course = course
+        insCourse.instructor = inst1
+        insCourse.save()
+
+        client = Client()
+        session = client.session
+        session['email'] = 'inst@uwm.edu'
+        session['type'] = 'instructor'
+        session.save()
+        response = client.get('/view_course_assignments/')
         # status code 200, we loaded the correct page
         self.assertEqual(response.status_code, 200)
         # since we returned a render, it has all the content of the page
         # we'll just look for the header
-        self.assertContains(response, "Assign Instructor")
+        self.assertContains(response, "Courses Assigned to You:")
         all_messages = [msg for msg in get_messages(response.wsgi_request)]
         # no error messages
-        self.assertEqual(len(all_messages), 0)
+        info_string = str(all_messages[0])
+        self.assertEqual(info_string, "COMPSCI: 301 \n")
+        self.assertEqual(len(all_messages), 1)
+
+    def test_add_two_course(self):
+        inst1 = models.User()
+        inst1.email = "inst@uwm.edu"
+        inst1.type = "instructor"
+        inst1.save()
+
+        course = models.Course()
+        course.course_department = "COMPSCI"
+        course.course_id = "301"
+        course.save()
+
+        course2 = models.Course()
+        course2.course_department = "COMPSCI"
+        course2.course_id = "351"
+        course2.save()
+
+        insCourse = models.InstructorCourse()
+        insCourse.course = course
+        insCourse.instructor = inst1
+        insCourse.save()
+
+        insCourse = models.InstructorCourse()
+        insCourse.course = course2
+        insCourse.instructor = inst1
+        insCourse.save()
+
+        client = Client()
+        session = client.session
+        session['email'] = 'inst@uwm.edu'
+        session['type'] = 'instructor'
+        session.save()
+        response = client.get('/view_course_assignments/')
+        # status code 200, we loaded the correct page
+        self.assertEqual(response.status_code, 200)
+        # since we returned a render, it has all the content of the page
+        # we'll just look for the header
+        self.assertContains(response, "Courses Assigned to You:")
+        all_messages = [msg for msg in get_messages(response.wsgi_request)]
+        # no error messages
+        info_string = str(all_messages[0])
+        self.assertEqual(info_string, "COMPSCI: 301 \nCOMPSCI: 351 \n")
+        self.assertEqual(len(all_messages), 1)
